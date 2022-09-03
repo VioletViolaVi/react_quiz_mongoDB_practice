9476075b06883607801291a747790ecea1779a7d
vivian <vivian.v18723@gmail.com>
2022-09-04 00:11:51 +0100

    import code for axios

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
 
 import App from "./App";
 import NavBar from "./components/NavBar";
 import "./main.css";
 
+// for axios use
+let url = "http://localhost:3000";
+axios.defaults.baseURL = url;
+axios.defaults.headers.post["Content-Type"] = "application/json";
+axios.defaults.headers.common["Authorization"] =
+  localStorage.getItem("JWT_PAYLOAD");
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-    <NavBar />
+      <NavBar />
       <App />
     </BrowserRouter>
   </React.StrictMode>
