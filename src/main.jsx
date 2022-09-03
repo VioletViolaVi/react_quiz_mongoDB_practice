b466ed86bb7796c8d1a8c5deb165ae81e89bb62f
vivian <vivian.v18723@gmail.com>
2022-09-03 18:52:44 +0100

    use BrowserRouter import NavBar

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import App from "./App";
+import NavBar from "./components/NavBar";
 import "./main.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <NavBar />
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
