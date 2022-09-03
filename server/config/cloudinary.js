596766e2196992a7ce74c55e0aa38d22057cea10
vivian <vivian.v18723@gmail.com>
2022-09-03 18:22:02 +0100

    create cloudinary.config

---
+++ server/config/cloudinary.js
@@ -0,0 +1,9 @@
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key: process.env.CLOUDINARY_APIKEY
+});
+
+module.exports = { cloudinary };
