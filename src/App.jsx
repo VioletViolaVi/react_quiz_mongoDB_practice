6e7f424ce7363c1b49a2ec01eda15b0e791987c3
vivian <vivian.v18723@gmail.com>
2022-09-03 18:52:27 +0100

    create Routes & Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import NotFound from "./components/NotFound";
+import Register from "./components/Register";
+
 function App() {
   return (
     <>
-      <h1>Hello</h1>
+      <Routes>
+        <Route path="*" element={<NotFound />}></Route>
+
+        <Route path="/" element={<Home />}></Route>
+        <Route path="register" element={<Register />}></Route>
+        <Route path="login" element={<Login />}></Route>
+      </Routes>
     </>
   );
 }
