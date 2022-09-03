1d5db2b11dd25a476d619a99c6aab817d9583e7a
vivian <vivian.v18723@gmail.com>
2022-09-04 00:13:31 +0100

    create & export Register component

--- src/components/Register/index.jsx
+++ src/components/Register/index.jsx
@@ -1,104 +1,96 @@
-import { useState } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
-function Register() {
-  // state variables
-  const [firstName, setFirstName] = useState("");
-  const [lastName, setLastName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      firstName: "",
+      lastName: "",
+      email: "",
+      password: ""
+    };
+  }
 
-  // sending form content to backend
-  const handleRegister = async (e) => {
-    e.preventDefault();
-
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
-        firstName,
-        lastName,
-        email,
-        password
-      })
+  render() {
+    // what user puts in first name input
+    const handleFirstName = (e) => {
+      return this.setState({ firstName: e.target.value });
     };
 
-    const response = await fetch("http://localhost:3000/register", options);
-    const data = await response.json();
-    console.log("data ===> ", data);
-  };
+    // what user puts in last name input
+    const handleLastName = (e) => {
+      return this.setState({ lastName: e.target.value });
+    };
 
-  // functions for setter functions
-  const handleFirstName = (e) => {
-    setFirstName((prevFirstName) => {
-      prevFirstName = e.target.value;
-      return prevFirstName;
-    });
-  };
+    // what user puts in email input
+    const handleEmail = (e) => {
+      return this.setState({ email: e.target.value });
+    };
 
-  const handleLastName = (e) => {
-    setLastName((prevLastName) => {
-      prevLastName = e.target.value;
-      return prevLastName;
-    });
-  };
+    // what user puts in password input
+    const handlePassword = (e) => {
+      return this.setState({ password: e.target.value });
+    };
 
-  const handleEmail = (e) => {
-    setEmail((prevEmail) => {
-      prevEmail = e.target.value;
-      return prevEmail;
-    });
-  };
+    // sends input info to backend
+    const handleBtnSend = () => {
+      return this.props.register({ ...this.state });
+    };
 
-  const handlePassword = (e) => {
-    setPassword((prevPassword) => {
-      prevPassword = e.target.value;
-      return prevPassword;
-    });
-  };
+    // stops page refreshing
+    const handleSubmitForm = (e) => {
+      e.preventDefault();
+    };
 
-  return (
-    <>
-      <h1>Register</h1>
-      <form onSubmit={handleRegister}>
-        <input
-          required
-          type="text"
-          placeholder="First Name"
-          value={firstName}
-          onChange={handleFirstName}
-        />
-        <input
-          required
-          type="text"
-          placeholder="Last Name"
-          value={lastName}
-          onChange={handleLastName}
-        />
-        <input
-          required
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={handleEmail}
-        />
-        <input
-          required
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={handlePassword}
-        />
-        <br />
-        <button>Register</button>
-      </form>
-      <p>
-        Have an account? <Link to={"/login"}>Login</Link>
-      </p>
-    </>
-  );
+    // // changes condition to 'true' in Auth class
+    // const handleLinkSwap = () => {
+    //   return this.setState({ tab: "login" });
+    // };
+
+    return (
+      <>
+        <h1>Register</h1>
+        <form onSubmit={handleSubmitForm}>
+          <input
+            required
+            type="text"
+            placeholder="First Name"
+            value={this.state.firstName}
+            onChange={handleFirstName}
+          />
+
+          <input
+            required
+            type="text"
+            placeholder="Last Name"
+            value={this.state.lastName}
+            onChange={handleLastName}
+          />
+
+          <input
+            required
+            type="email"
+            placeholder="Email"
+            value={this.state.email}
+            onChange={handleEmail}
+          />
+          <input
+            required
+            type="password"
+            placeholder="Password"
+            value={this.state.password}
+            onChange={handlePassword}
+          />
+          <br />
+          <button onClick={handleBtnSend}>Register</button>
+        </form>
+        {/* <p onClick={handleLinkSwap}>
+          Have an account? <Link to={"/login"}>Login</Link>
+        </p> */}
+      </>
+    );
+  }
 }
 
 export default Register;
